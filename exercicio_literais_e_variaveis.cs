0e48046eca5634f8a39a3c107d93c564af55cb37
Júlio César Misnerovicz Klamt <105139477+Polacovicz@users.noreply.github.com>
2024-09-17 14:19:03 -0300

    exercicio_literais_e_variaveis.cs
    exercicio sobre literais e variaveis em c#

---
+++ exercicio_literais_e_variaveis.cs
@@ -0,0 +1,11 @@
+// Exibir valores literais e variáveis
+// Bob
+// 3
+// 34.4
+
+
+string nome = "Bob";
+int numero = 3;
+float numero2 = 34.4F;
+
+Console.WriteLine(" Hello " + nome + " You have " + numero + " messages in your inbox. The temperature is " + numero2 + " celsius");
