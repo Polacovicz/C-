ca6b5a56141a5fc06c79e13657c4b9dd8136fc57
Júlio César Misnerovicz Klamt <105139477+Polacovicz@users.noreply.github.com>
2024-09-17 14:09:55 -0300

    variaveis.cs
    Variaveis e operadores lógicos.

---
+++ variaveis.cs
@@ -0,0 +1,29 @@
+Console.WriteLine('b');
+Console.WriteLine(0.25F);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+//string para palavras, frases ou qualquer dado alfanumérico para apresentação, não cálculo
+//char para um caractere alfanumérico
+//int para um número inteiro
+//decimal para um número com um componente frácionario
+// bool para um valor true/false
+
+string firstName;
+char userOpetion;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabela";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+var message = "Hello world!";
+Console.WriteLine(message);
